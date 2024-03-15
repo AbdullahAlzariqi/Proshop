b91183542deaa1722d3861bee5d21e5bdf4652b3
AbdullahAlzariqi <abdallahtalat5597@gmail.com>
2024-03-15 22:22:38 +0300

    Product API slice

--- frontend/src/components/HomeScreen.jsx
+++ frontend/src/components/HomeScreen.jsx
@@ -1,29 +1,25 @@
+
 import { Row, Col } from 'react-bootstrap'
-import Product from './Product'
-import { useGetProductsQuery } from '../slices/productApiSlice'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
 const HomeScreen = () => {
-    const { data: products, isLoading, error } = useGetProductsQuery();
+    const { data: products, isLoading, isError } = useGetProductsQuery();
+    console.log(products)
+
+
+
 
 
     return (
         <>
-            {isLoading ? (
-                <h2>...Loading</h2>
-            ) : error ? (<div>
-                {error?.data?.message || error.error}
-            </div>) : (<>
-                <h1>Latest Products</h1>
-                <Row>
-                    {
-                        products.map((product) => {
-                            return <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                <Product product={product} />
-                            </Col>
-                        })
-                    }
-                </Row>
-            </>)}
-
+            <Row>
+                {
+                    products.map((product) => {
+                        return <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    })
+                }
+            </Row>
         </>
     )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./slices/apiSlice";
+import { apiSlice } from "./slices/sliceApi";
 
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
     },
-
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
 });
 
 export default store;
